8c9e4fd210882a7a336e423daeb81be46f37af1a
Prakash Kumar Thuthika <prakash.thuthika@gmail.com>
2025-02-10 19:32:40 +0530

    Create loopfiles.py

---
+++ loopfiles.py
@@ -0,0 +1,13 @@
+
+##import required directories
+import os
+
+#root directory
+directory = "C:/direct/fold"
+
+for foldername, subfolders, filenames in os.walk(directory):
+    for filename in filenames:
+        if filename.lower().endswith(".pdf"): # check if its pdf file
+            print(os.path.join(foldername, filename)) # print path and file name
+
+    
